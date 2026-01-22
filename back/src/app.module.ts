566e3ac93bb2b733773a77d4754c86898e28de33
dev-brunosantos <brunosdesenvolvedor@gmail.com>
2026-01-22 17:53:20 -0300

    Atualização automatica do codigo

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbService } from './db/db.service';
 import { DbModule } from './db/db.module';
+import { CargosModule } from './cargos/cargos.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { DocumentosModule } from './documentos/documentos.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, CargosModule, UsuariosModule, DocumentosModule],
   controllers: [AppController],
   providers: [AppService, DbService],
 })
